13dba9e65b3d6eea8b001b81ea4eb8597ae9c3b0
Franklinson <franklinhassey@gmail.com>
2026-08-14 08:33:58 +0000

    Resolve Railway env variable names before S3Boto3Storage init

--- django_railway_storage/backends.py
+++ django_railway_storage/backends.py
@@ -5,16 +5,20 @@ import os
 from botocore.exceptions import ClientError
 
 
-def _get_setting(django_setting, *env_keys, default=None):
-    """Read from Django settings first, then fall back to Railway env var names."""
-    value = getattr(settings, django_setting, None)
-    if value:
-        return value
-    for key in env_keys:
-        value = os.environ.get(key)
-        if value:
-            return value
-    return default
+def _resolve_settings():
+    """Map Railway's native env variable names to django-storages expected names."""
+    mapping = {
+        'AWS_STORAGE_BUCKET_NAME': ('AWS_STORAGE_BUCKET_NAME', 'AWS_S3_BUCKET_NAME'),
+        'AWS_S3_ENDPOINT_URL': ('AWS_S3_ENDPOINT_URL', 'AWS_ENDPOINT_URL'),
+        'AWS_S3_REGION_NAME': ('AWS_S3_REGION_NAME', 'AWS_DEFAULT_REGION'),
+    }
+    for django_key, env_keys in mapping.items():
+        if not getattr(settings, django_key, None):
+            for env_key in env_keys:
+                value = os.environ.get(env_key)
+                if value:
+                    setattr(settings, django_key, value)
+                    break
 
 
 class PrivateMediaStorage(S3Boto3Storage):
@@ -26,22 +30,25 @@ class PrivateMediaStorage(S3Boto3Storage):
     querystring_expire = 3600
     addressing_style = 'path'
 
+    def __init__(self, *args, **kwargs):
+        _resolve_settings()
+        super().__init__(*args, **kwargs)
+
     def url(self, name, parameters=None, expire=None, http_method=None):
         if expire is None:
             expire = self.querystring_expire
         try:
             s3_client = boto3.client(
                 's3',
-                aws_access_key_id=_get_setting('AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY_ID'),
-                aws_secret_access_key=_get_setting('AWS_SECRET_ACCESS_KEY', 'AWS_SECRET_ACCESS_KEY'),
-                endpoint_url=_get_setting('AWS_S3_ENDPOINT_URL', 'AWS_ENDPOINT_URL'),
-                region_name=_get_setting('AWS_S3_REGION_NAME', 'AWS_DEFAULT_REGION', default='us-east-1'),
+                aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+                region_name=getattr(settings, 'AWS_S3_REGION_NAME', 'us-east-1'),
                 config=boto3.session.Config(signature_version='s3v4', s3={'addressing_style': 'path'}),
             )
-            bucket = _get_setting('AWS_STORAGE_BUCKET_NAME', 'AWS_S3_BUCKET_NAME')
             return s3_client.generate_presigned_url(
                 'get_object',
-                Params={'Bucket': bucket, 'Key': name},
+                Params={'Bucket': settings.AWS_STORAGE_BUCKET_NAME, 'Key': name},
                 ExpiresIn=expire,
             )
         except ClientError:
@@ -54,3 +61,7 @@ class PublicMediaStorage(S3Boto3Storage):
     file_overwrite = False
     custom_domain = False
     addressing_style = 'path'
+
+    def __init__(self, *args, **kwargs):
+        _resolve_settings()
+        super().__init__(*args, **kwargs)
